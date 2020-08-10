0f36d9f14e48cd681292714130b4a4c4ea3c4e84
Pouria-t <cloner.1383@gmail.com>
2020-08-10 23:08:36 +0430

    add config program

---
+++ config.py
@@ -0,0 +1,9 @@
+# This is a configured instance that you can edit to your liking
+
+# login cedentials
+USERNAME = 'admin'
+PASSWORD = 'pass'
+
+#this config SECRET_KEY
+
+SECRET_KEY = 'top secret '
